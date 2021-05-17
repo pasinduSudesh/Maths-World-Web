58d13b72b801bfb0e09ee221482132d32f2aec8b
pasinduSudesh <sudeshdilshan.17@cse.mrt.ac.lk>
2021-05-17 18:49:43 +0530

    added page not found routes

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -9,14 +9,15 @@ import { SignupComponent } from "../app/signup/signup.component";
 import { adminRoutes } from './admin/admin-routing';
 import { studentRoutes } from './student/student-routing';
 import { UserDetailsComponent } from './admin/user-details/user-details.component';
-
+import { PageNotFoundComponent } from './page-not-found/page-not-found.component';
 
 const routes: Routes = [
   {path: "", component: HomeComponent},
   {path: "signupEmpty", component: SignupComponent},
   {path: "addEditorDetails", component: UserDetailsComponent},
   {path: "admin", children: adminRoutes},
-  {path: "", children: studentRoutes}
+  {path: "", children: studentRoutes},
+  {path: "**", component: PageNotFoundComponent}
 ];
 
 @NgModule({
