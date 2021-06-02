1283a94060871c4b18bec4e50edd78bf977c68d0
Chamupathi Mendis <chamupathi2008@gmail.com>
2021-06-02 10:58:11 +0530

    ui fixing

---
+++ src/app/navbar-create-account/navbar-create-account.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-navbar-create-account',
+  templateUrl: './navbar-create-account.component.html',
+  styleUrls: ['./navbar-create-account.component.scss']
+})
+export class NavbarCreateAccountComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
