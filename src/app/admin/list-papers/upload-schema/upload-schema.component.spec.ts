ef2ffb3748cdae0b9e13edc268341b452af70e11
pasinduSudesh <sudeshdilshan.17@cse.mrt.ac.lk>
2021-07-18 22:42:53 +0530

    added upload schema

---
+++ src/app/admin/list-papers/upload-schema/upload-schema.component.spec.ts
@@ -0,0 +1,25 @@
+// import { ComponentFixture, TestBed } from '@angular/core/testing';
+
+// import { UploadSchemaComponent } from './upload-schema.component';
+
+// describe('UploadSchemaComponent', () => {
+//   let component: UploadSchemaComponent;
+//   let fixture: ComponentFixture<UploadSchemaComponent>;
+
+//   beforeEach(async () => {
+//     await TestBed.configureTestingModule({
+//       declarations: [ UploadSchemaComponent ]
+//     })
+//     .compileComponents();
+//   });
+
+//   beforeEach(() => {
+//     fixture = TestBed.createComponent(UploadSchemaComponent);
+//     component = fixture.componentInstance;
+//     fixture.detectChanges();
+//   });
+
+//   it('should create', () => {
+//     expect(component).toBeTruthy();
+//   });
+// });
